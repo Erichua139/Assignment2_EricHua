24d73f40c62dafe50a2cd5503977fa69461ad887
Erichua139 <94157031+Erichua139@users.noreply.github.com>
2021-12-02 13:01:38 -0500

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,31 +1,37 @@
- import java.util.HashSet;
+import java.util.HashSet;
 import java.util.Vector;
 
 public class Department {
-   private String name; // the name of school Dept of Computing and Info Science
-   private String id; // short name for courses SOFE, ELEE, STAT, etc
-   private Vector<Course> courseList; // all courses offered by the department
-   private Vector<Student> registerList; // all students taking courses in the department.
+    private String name; // the name of school Dept of Computing and Info Science
+    private String id; // short name for courses SOFE, ELEE, STAT, etc
+    private Vector<Course> courseList; // all courses offered by the department
+    private Vector<Student> registerList; // all students taking courses in the department.
 
-   
-   public Department(String name, String id) {
-	      // also initialize the vectors
-	   
-	   }
 
-   public String getName() {
-	   
-   }
+    public Department(String name, String id) {
+        // also initialize the vectors
+        this.name = name;
+        this.id=id;
+        courseList=new Vector();
+        registerList = new Vector();
+    }
 
-   public String getId() {
-	  
-   }
-   
- 	
-   public String toString() {
-      // returns a string representation of department name, number 
-      // of courses offered and number of students registered in the
-      // department. Use the format:
-      // ECSE: 53 courses, 460 students     
-   }
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    public String toString() {
+        // returns a string representation of department name, number
+        // of courses offered and number of students registered in the
+        // department. Use the format:
+        // ECSE: 53 courses, 460 students
+        String temp="";
+        temp += name+'\n'+id+": "+courseList.size()+" courses, "+registerList.size()+" students";
+        return temp;
+    }
 }
