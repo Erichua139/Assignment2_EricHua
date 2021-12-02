b336fbfb46fff0dfc0a5e1a1d73fdc8124f5d5b8
Erichua139 <94157031+Erichua139@users.noreply.github.com>
2021-12-01 23:29:17 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,27 +1,34 @@
-public class Person {
-   private String   name;
-   
-   
-   public Person(String initialName)
-   {
-      
-   }
-   
-   public Person()
-   {
-     
-   }
-   
-   public void setName( String fullName) {
+import java.util.Scanner;
 
+public class Person {
+    private String   name;
+
+
+    public Person(String initialName)
+    {
+        this.name= initialName;
     }
 
-   public String getName() {
-      return null;
-   }
- 
-   public String toString() {
-   return null;
+    public Person()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a name");
+        this.name=in.nextLine();
+    }
 
-   }
+
+    public void setName( String fullName) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the new name:");
+        this.name=in.nextLine();
+    }
+
+    public String getName() {
+        return (name);
+    }
+
+    public String toString() {
+        return (name)
+
+    }
 }
